a7dd9d55a9be12adf801a2f28dc2ab194a2d8ca3
DodoLaRacaille <106687159+DodoLaRacaille@users.noreply.github.com>
2024-03-25 21:05:26 +0100

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data.json';
+import Header from './components/header/header'
 
-function App() {
+interface YourData {
+  ItemName: string;
+  ItemImage: string;
+  ItemCraft: string;
+}
+
+function ItemLines() {
+  const [items, setItems] = useState<YourData[]>(data);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  // Filtrer les éléments en fonction de la chaîne de recherche
+  const filteredItems = items.filter(item =>
+    item.ItemName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <SearchBar searchTerm={searchTerm} handleSearch={handleSearch} />
+      {filteredItems.map((item, index) => (
+        <ItemCard key={index} data={item} />
+      ))}
     </div>
   );
 }
 
-export default App;
+function ItemCard({ data }: { data: YourData }) {
+  const [showCraftImage, setShowCraftImage] = useState(false);
+
+  const toggleCraftImage = () => {
+    setShowCraftImage(!showCraftImage);
+  };
+
+  const imagePath = `/items/${data.ItemImage}`;
+  const imageCraft = `/crafts/${data.ItemCraft}`;
+  return (
+    <div className="item" onMouseEnter={toggleCraftImage} onMouseLeave={toggleCraftImage}>
+      <img className="itemImage" src={imagePath} alt={data.ItemName} />
+      <p className="itemName">{data.ItemName}</p>
+      {showCraftImage && <img className="craftImage" src={imageCraft}/>}
+    </div>
+  );
+}
+
+// Composant SearchBar pour la recherche
+function SearchBar({
+  searchTerm,
+  handleSearch
+}: {
+  searchTerm: string;
+  handleSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}) {
+  return (
+    <div className="searchBar">
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+    </div>
+  );
+}
+
+export default ItemLines;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+    return (
+      <h1>Items !</h1>
+    );
+  }
+
+  export default Header;
